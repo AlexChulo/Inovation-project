e37b7adab4c3de83c97059117df2ae10875c4339
AlexChulo <alex123149@gmail.com>
2025-01-22 14:09:23 +0100

    Three.js + vite start

---
+++ main.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
+
+
+const gui = new GUI();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const sun = new THREE.DirectionalLight();
+sun.position.set(1, 2, 3);
+scene.add(sun);
+
+const ambient = new THREE.AmbientLight();
+ambient.intensity = 0.5;
+scene.add(ambient);
+
+camera.position.z = 5;
+
+const stats = new Stats();
+document.body.appendChild( stats.dom );
+
+
+function animate() {
+	
+    stats.update();
+	renderer.render( scene, camera );
+
+}
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const folder = gui.addFolder('cube')
+folder.add(cube.position, 'x', -2, 2, 0.1).name('position x');
+folder.addColor(cube.material, 'color');
